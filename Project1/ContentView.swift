ae3cbf855a0d6085b74d17932e424d10c0591bcb
pieterjooste <pieter.jooste.jp@gmail.com>
2024-06-03 09:07:20 +0200

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Pieter Jooste on 2024/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Pieter Jooste on 2024/06/03.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
